50839d2c94c2109da1e2059f957b9db09019fd66
MBAIRAMADJI <mbairamadjieric@gmail.com>
2018-03-06 00:32:08 +0100

    refactoring

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
-  }
-});
+// app.use(function (req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     next();
+//   } else {
+//     res.redirect('http://' + req.hostname + req.url);
+//   }
+// });
 
 
 app.listen(PORT, function() {
